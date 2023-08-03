5fcda8a802a1ec16839639e22be0667188c292a8
ysmain <ys_main06350@yahoo.co.jp>
2023-08-03 16:56:31 +0900

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,7 +84,7 @@
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     {{-- future: admin 確認 --}}
-                                    @if(Auth::user()->id === 1)
+                                    @if(Auth::user()->role_id === 1)
                                         <a href="{{route('admin.users.index')}}" class="dropdown-item">
                                             <i class="fa-solid fa-user-gear"></i> Admin
                                         </a>
