04f6be84967b0dd5ea9f35c8aeb6d7861e0cb5ff
ysmain <ys_main06350@yahoo.co.jp>
2023-08-04 14:00:27 +0900

    added

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        User::Create([
+            "name" => "admin",
+            "email" => "admin@gmail.com",
+            "password" => Hash::make("password"),
+            "description" => "admin account",
+            "role_id" => 1
+        ]);
+    }
+}
